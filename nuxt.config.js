7f7db7c58c047702364219356e15536db05fb937
Santiago Marcano <santiagomarcano@Santiagos-MacBook-Air.local>
2021-02-17 20:59:20 +0100

    clean

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,21 @@
 import en from './assets/i18n/en.json'
 export default {
+  target: 'static',
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    // title: process.env.npm_package_name || '',
+    // meta: [
+    //   { charset: 'utf-8' },
+    //   { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    //   {
+    //     hid: 'description',
+    //     name: 'description',
+    //     content: process.env.npm_package_description || '',
+    //   },
+    // ],
+    // link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
    ** Customize the progress-bar color
